9082a80ea0dbc95afad068d6343b5e8e27ed9cc6
Saurabh <ss11485@nyu.edu>
2018-10-14 19:17:12 -0400

    Modified test_server file for Products class

--- tests/test_server.py
+++ tests/test_server.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 """
-Pet API Service Test Suite
+Product API Service Test Suite
 
 Test cases can be run with the following:
   nosetests -v --with-spec --spec-color
@@ -28,16 +28,16 @@ import logging
 from flask_api import status    # HTTP Status Codes
 from mock import MagicMock, patch
 
-from app.models import Pet, DataValidationError, db
-import app.service as service
+from app.productmodel import Product, ValidationError, db
+import app.productservice as service
 
 DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///../db/test.db')
 
 ######################################################################
 #  T E S T   C A S E S
 ######################################################################
-class TestPetServer(unittest.TestCase):
-    """ Pet Server Tests """
+class TestProductServer(unittest.TestCase):
+    """ Product Server Tests """
 
     @classmethod
     def setUpClass(cls):
@@ -56,8 +56,8 @@ class TestPetServer(unittest.TestCase):
         service.init_db()
         db.drop_all()    # clean up the last tests
         db.create_all()  # create new tables
-        Pet(name='fido', category='dog', available=True).save()
-        Pet(name='kitty', category='cat', available=True).save()
+        Product(pid=1,pname='Athens Table', pdesc='Stupid Table', pcat="Table", pcond="Boxed", pprice=20, prat=5, prev="").save()
+        Product(pid=1,pname='Rome Chair', pdesc='Stupid Chair', pcat="Chair", pcond="Boxed", pprice=40, prat=8, prev="").save()
         self.app = service.app.test_client()
 
     def tearDown(self):
@@ -69,38 +69,38 @@ class TestPetServer(unittest.TestCase):
         resp = self.app.get('/')
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = json.loads(resp.data)
-        self.assertEqual(data['name'], 'Pet Demo REST API Service')
+        self.assertEqual(data['name'], 'Product Demo REST API Service')
 
-    def test_get_pet_list(self):
-        """ Get a list of Pets """
+    def test_get_product_list(self):
+        """ Get a list of Products """
         resp = self.app.get('/pets')
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = json.loads(resp.data)
         self.assertEqual(len(data), 2)
 
-    def test_get_pet(self):
-        """ Get a single Pet """
+    def test_get_product(self):
+        """ Get a single Product """
         # get the id of a pet
-        pet = Pet.find_by_name('fido')[0]
-        resp = self.app.get('/pets/{}'.format(pet.id),
+        pet = Product.find_by_name('fido')[0]
+        resp = self.app.get('/products/{}'.format(Product.id),
                             content_type='application/json')
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = json.loads(resp.data)
-        self.assertEqual(data['name'], pet.name)
+        self.assertEqual(data['name'], Product.name)
 
-    def test_get_pet_not_found(self):
-        """ Get a Pet thats not found """
-        resp = self.app.get('/pets/0')
+    def test_get_product_not_found(self):
+        """ Get a Product thats not found """
+        resp = self.app.get('/products/0')
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
 
-    def test_create_pet(self):
-        """ Create a new Pet """
+    def test_create_product(self):
+        """ Create a new Product """
         # save the current number of pets for later comparison
-        pet_count = self.get_pet_count()
+        product_count = self.get_product_count()
         # add a new pet
-        new_pet = dict(name='sammy', category='snake', available=True)
-        data = json.dumps(new_pet)
-        resp = self.app.post('/pets',
+        new_product = dict(pid=1,pname='Greek Table', pdesc='Its a Table', pcat="Table", pcond="Boxed", pprice=12, prat=5, prev="")
+        data = json.dumps(new_product)
+        resp = self.app.post('/products',
                              data=data,
                              content_type='application/json')
         self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
@@ -109,50 +109,50 @@ class TestPetServer(unittest.TestCase):
         self.assertTrue(location != None)
         # Check the data is correct
         new_json = json.loads(resp.data)
-        self.assertEqual(new_json['name'], 'sammy')
+        self.assertEqual(new_json['name'], 'Greek Table')
         # check that count has gone up and includes sammy
-        resp = self.app.get('/pets')
+        resp = self.app.get('/products')
         # print 'resp_data(2): ' + resp.data
         data = json.loads(resp.data)
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(data), pet_count + 1)
+        self.assertEqual(len(data), product_count + 1)
         self.assertIn(new_json, data)
 
-    def test_update_pet(self):
-        """ Update an existing Pet """
-        pet = Pet.find_by_name('kitty')[0]
-        new_kitty = dict(name='kitty', category='tabby', available=True)
-        data = json.dumps(new_kitty)
-        resp = self.app.put('/pets/{}'.format(pet.id),
+    def test_update_product(self):
+        """ Update an existing Product """
+        product = Product.find_by_name('Greek Table')[0]
+        new_product = dict(pid=1,pname='Greek Table', pdesc='Its a Table', pcat=" Fancy Table", pcond="Boxed", pprice=12, prat=5, prev="")
+        data = json.dumps(new_product)
+        resp = self.app.put('/pets/{}'.format(product.id),
                             data=data,
                             content_type='application/json')
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         new_json = json.loads(resp.data)
-        self.assertEqual(new_json['category'], 'tabby')
+        self.assertEqual(new_json['category'], 'Fancy Table')
 
-    def test_delete_pet(self):
-        """ Delete a Pet """
-        pet = Pet.find_by_name('fido')[0]
-        # save the current number of pets for later comparrison
-        pet_count = self.get_pet_count()
-        resp = self.app.delete('/pets/{}'.format(pet.id),
+    def test_delete_product(self):
+        """ Delete a Product """
+        product = Product.find_by_name('Greek Table')[0]
+        # save the current number of products for later comparrison
+        product_count = self.get_product_count()
+        resp = self.app.delete('/pets/{}'.format(product.id),
                                content_type='application/json')
         self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(len(resp.data), 0)
-        new_count = self.get_pet_count()
-        self.assertEqual(new_count, pet_count - 1)
+        new_count = self.get_product_count()
+        self.assertEqual(new_count, product_count - 1)
 
     def test_query_pet_list_by_category(self):
-        """ Query Pets by Category """
-        resp = self.app.get('/pets',
+        """ Query Products by Category """
+        resp = self.app.get('/products',
                             query_string='category=dog')
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         self.assertGreater(len(resp.data), 0)
-        self.assertIn('fido', resp.data)
-        self.assertNotIn('kitty', resp.data)
+        self.assertIn('Athens Table', resp.data)
+        self.assertNotIn('Table', resp.data)
         data = json.loads(resp.data)
         query_item = data[0]
-        self.assertEqual(query_item['category'], 'dog')
+        self.assertEqual(query_item['category'], 'Fancy Table')
 
     # @patch('service.Pet.find_by_name')
     # def test_bad_request(self, bad_request_mock):
@@ -173,9 +173,9 @@ class TestPetServer(unittest.TestCase):
 # Utility functions
 ######################################################################
 
-    def get_pet_count(self):
-        """ save the current number of pets """
-        resp = self.app.get('/pets')
+    def get_product_count(self):
+        """ save the current number of products """
+        resp = self.app.get('/products')
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = json.loads(resp.data)
         return len(data)
