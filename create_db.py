5824056f252524db42c7307d449b49676c0d57a6
Richa Deshmukh <rsd352@nyu.edu>
2018-11-09 19:21:55 -0500

    changed db value for review

--- create_db.py
+++ create_db.py
@@ -5,7 +5,7 @@ def dbcreate():
     Product.logger.info(Product.query.with_entities(Product.id).all())
     if((Product.query.with_entities(Product.id).all()) != []):
         return
-    Product(1, 'Desk', 'Wooden study desk', 'Furniture', 50, 'Boxed', 500, " ", 8).save()
+    Product(1, 'Desk', 'Wooden study desk', 'Furniture', 50, 'Boxed', 500, "Great product", 8).save()
     Product(2, 'Chair', 'Mesh office chair', 'Furniture', 30, 'Boxed', 1500, " ", 9).save()
     Product(3, 'Tshirt', 'Black printed tshirt', 'Clothing', 20, 'Boxed', 900, " ", 8).save()
     Product(4, 'Trousers', 'Brown linen trouser', 'Clothing', 35, 'Boxed', 300, " ", 10).save()
