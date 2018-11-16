9b1b4fbafc01f5dfd2c6bcb88dafb2088100dacb
Saurabh Sharan <ss11485@nyu.edu>
2018-11-15 19:06:54 -0500

    Uncommented original code for nosetests

--- app/service.py
+++ app/service.py
@@ -55,19 +55,21 @@ def internal_server_error(error):
 @app.route("/")
 def index():
     app.logger.info(Product.query.all())
-    # return jsonify(name="Product Demo REST API Service",
-    #                version='1.0', Get_All_Products="[GET] /products",
-    #                Get_Latest_Products="[GET] /products/latest",
-    #                Get_Product_By_ID="[GET] /products?id=<item_id>",
-    #                Get_Product_By_Category="[GET] /products?category=<category>",
-    #                Get_Product_By_Name="[GET] /products?name=<name>",
-    #                Get_Product_By_PriceRange="[GET] /products/pricerange?minimum=<min-price>&maximum=<max_price>",
-    #                Create_Product="[POST] /products/",
-    #                Update_Product="[PUT] /products/<item_id>",
-    #                Update_Product_Rating="[PUT] /products/rating/<item_id>?stars=<rating from [1,10]>",
-    #                Delete_Product="[DELETE] /products/<item_id>"
-    #                ), status.HTTP_200_OK
-    return app.send_static_file('index.html')
+    return jsonify(name="Product Demo REST API Service",
+                   version='1.0', Get_All_Products="[GET] /products",
+                   Get_Latest_Products="[GET] /products/latest",
+                   Get_Product_By_ID="[GET] /products?id=<item_id>",
+                   Get_Product_By_Category="[GET] /products?category=<category>",
+                   Get_Product_By_Name="[GET] /products?name=<name>",
+                   Get_Product_By_PriceRange="[GET] /products/pricerange?minimum=<min-price>&maximum=<max_price>",
+                   Create_Product="[POST] /products/",
+                   Update_Product="[PUT] /products/<item_id>",
+                   Update_Product_Rating="[PUT] /products/rating/<item_id>?stars=<rating from [1,10]>",
+                   Delete_Product="[DELETE] /products/<item_id>",
+                   Delete_All_Products="[DELETE] /products"
+                   ), status.HTTP_200_OK
+    #Please comment above return statement and uncommment the below return statement FOR BEHAVIORAL TESTING
+    # return app.send_static_file('index.html')
 #########################
 # list all products
 #########################
