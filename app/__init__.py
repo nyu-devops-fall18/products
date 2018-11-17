b8117b04761dadb763ebf833f1df0b6db6a9dc79
Richa Deshmukh <rsd352@nyu.edu>
2018-11-17 15:23:34 -0500

    gunicorn error fixed

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,11 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'Shhhh'
 app.config['LOGGING_LEVEL'] = logging.INFO
 
+import os
+import service, model
+DEBUG = (os.getenv('DEBUG', 'False') == 'True')
+PORT = os.getenv('PORT', '5000')
+
 # Set up logging for production
 print('Setting up logging for {}...'.format(__name__))
 if __name__ != '__main__':
@@ -18,3 +23,8 @@ if __name__ != '__main__':
         app.logger.setLevel(gunicorn_logger.level)
 
 app.logger.info('Logging established')
+app.logger.info("**********************************************")
+app.logger.info(" P R O D U C T   S E R V I C E   R U N N I N G")
+app.logger.info("**********************************************")
+service.init_db()
+# app.run(host='0.0.0.0', port=int(PORT), debug=DEBUG)
