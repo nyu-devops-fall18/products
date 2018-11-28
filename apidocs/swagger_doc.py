c89b2b7cc5292ae9a3ebf5f3afe2f5b196fd59d0
Saumya <sg5290@nyu.edu>
2018-11-28 00:36:40 -0500

    Added ratings to Swagger

--- apidocs/swagger_doc.py
+++ apidocs/swagger_doc.py
@@ -1,36 +1,36 @@
-
 from flask import Flask
 from flask_restplus import Api, Resource, fields, reqparse
-#from app.model import Product
+# from app.model import Product
 
 app = Flask(__name__)
 api = Api(app, version='1.0', title='Sample API for NYU-DevOps-Products')
 
-product_model = api.model('Product', {'id': fields.String(required=True, description='The id of the product'),
+product_model = api.model('Product', {'id': fields.Integer(required=True, description='The id of the product'),
                               'name': fields.String(required=True, description='The name of the product'),
                               'description': fields.String(required=True, description='Detailed information about the product'),
                               'category': fields.String(required=True, description='The category of the product'),
-                              'price': fields.String(required=True, description='The price of the product'),
+                              'price': fields.Integer(required=True, description='The price of the product'),
                               'condition': fields.String(required=True, description='The condition of the product'),
-                              'inventory': fields.String(required=True, description='The inventory of the product'),
+                              'inventory': fields.Integer(required=True, description='The inventory of the product'),
                               'review': fields.String(required=False, description='The review of the product'),
-                              'stars': fields.String(required=True, description='The rating of the product'),
-                              'updateddate': fields.String(required=False),
-                              'updateCount': fields.String(required=False, description='The most recent count of the product'),
-                              'totalrating': fields.String(required=False, description='The total rating of the product')
+                              'rating': fields.Integer(required=True, description='The rating of the product'),
+                              'hitCount': fields.Integer(required=True, description='The number of times product has been rated')
                              })
 
-products = [{'id': 1, 'name': 'Desk', 'description': 'wooden study desk', 'category': 'Furniture', 'price': '50', 'condition': 'Boxed', 'inventory': '500', 'review': 'Great product', 'stars': '8'}]
-product = {'id': 2, 'name': 'Chair', 'description': 'mesh office chair', 'category': 'Furniture', 'price': '30', 'condition': 'Boxed', 'inventory': '1500', 'review': '', 'stars': '9'}
-products.append(product)
+products = []
+product1 = {'id': 1, 'name': 'Desk', 'description': 'wooden study desk', 'category': 'Furniture', 'price': '50', 'condition': 'Boxed', 'inventory': '500', 'review': 'Great product', 'rating': '8','hitCount':'1'}
+product2 = {'id': 2, 'name': 'Chair', 'description': 'mesh office chair', 'category': 'Furniture', 'price': '30', 'condition': 'Boxed', 'inventory': '1500', 'review': '', 'rating': '9','hitCount':'1'}
+products.append(product1)
+products.append(product2)
+results = []
 
 product_arguments = reqparse.RequestParser()
 product_arguments.add_argument('minimum', type=int, required=True)
 product_arguments.add_argument('maximum', type=int, required=True)
 
 product_arguments1 = reqparse.RequestParser()
-product_arguments.add_argument('id', type=int, required=True)
-product_arguments.add_argument('rating', type=int, required=True, choices=[1,2,3,4,5,6,7,8,9,10])
+product_arguments1.add_argument('id', type=int, required=True)
+product_arguments1.add_argument('stars', type=int, required=True, choices=[1,2,3,4,5,6,7,8,9,10])
 
 
 @api.route('/products')
@@ -40,59 +40,100 @@ class ProductCollection(Resource):
         return products, 200
 
     @api.expect(product_model)
-
     def put(self):
-        return products, 202
+        return api.payload, 200
 
+    @api.expect(product_model)
     def post(self):
         new_product = api.payload
         products.append(new_product)
         return new_product, 201
 
     def delete(self):
-	return products, 204
+	    return [], 204
 
 
-@api.route('/products?id=id')
+@api.route('/products/<id>')
 @api.doc(params={'id':'product id'})
-class ProductCOllection(Resource):
+class ProductCollection(Resource):
     def get(self, id):
-	return products, 200
-
-    def put(self, id):
-	return products, 202
-
-    def delete(self, id):
-	return products, 204
-
-
-@api.route('/products?name=name')
-@api.doc(params={'name':'product name'})
-class ProductCOllection(Resource):
-    def get(self, name):
-	return products, 200
+        global results
+        results2 = []
+        for p in products:
+            paramid = int(id)
+            pid = int(p['id'])
+            if(pid == paramid):
+                print("Yayy")
+                print((id))
+                results2.append(p)
+        return results2, 200
 
 
-@api.route('/products?category=category')
-@api.doc(params={'category':'product category'})
-class ProductCOllection(Resource):
-    def get(self, category):
-	return products, 200
-
-
-@api.route('/products/pricerange')
-class ProductCOllection(Resource):
+    # @api.route('/products', methods=['GET'])
+    # @api.doc(params={'name':'product name', 'category':'product category'})
+    # class ProductCollection(Resource):
+    #     def get(self, name):
+    #         return api.output(), 200
+    #
+    #
+    # @api.route('/products', methods=['GET'])
+    # @api.doc(params={'category':'product category'})
+    # class ProductCollection(Resource):
+    #     def get(self, category):
+    #         return api.output(), 200
+    #
+    #
+@api.route('/products/pricerange', methods=['GET'])
+class ProductCollection(Resource):
     @api.expect(product_arguments, validate=True)
     def get(self):
-	return products, 200
+        # print((product_arguments.parse_args())['minimum'])
+        # print((product_arguments.parse_args())['maximum'])
+        min = int((product_arguments.parse_args())['minimum'])
+        max = int ((product_arguments.parse_args())['maximum'])
+        global results
+        results1=[]
+        for p in products:
+            print(min)
+            price1 = int(p['price'])
+            print(price1)
+            print(max)
+            if (price1 >= min):
+                print ("yay1")
+                if (price1 <= max):
+                    results1.append(p)
+                    print("Yayy")
+                print(results1)
+        return results1, 200
+    #
+    #
 
-
-#@api.route('/products/rating/1', methods=['PUT'])
-#@api.doc(params={'stars':'product rating'})
-#class ProductCOllection(Resource):
-    #@api.expect(product_arguments1, validate=True)
-#    def put(self):
-#	return products, 202
+@api.route('/products/rating/<int:id1>/<int:star1>')
+@api.param('id1', 'The Pet identifier')
+@api.param('star1', 'The Pet identifier')
+# @api.doc(params={'stars':'product rating'})
+class ProductCollection(Resource):
+    # @api.expect(product_arguments1, validate=True)
+    def put(self, id1,star1):
+        # print(product_arguments1)
+        # p_id = int((product_arguments1.parse_args())['id'])
+        # p_stars = int ((product_arguments1.parse_args())['stars'])
+        print (id1)
+        print (star1)
+        product1 = []
+        for p in products:
+            pid = int(p['id'])
+            print(pid)
+            if (pid == id1):
+                product1 = p
+                break
+        rating1 = int (product1['rating'])
+        hit= int(product1['hitCount'])
+        hit+=1
+        product1['hitCount']=hit
+        print (int (product1['hitCount']))
+        product1['rating']= int (rating1 + star1)/ int(product1['hitCount'])
+        return product1,201
 
 
 #@api.route('/products/latest')
