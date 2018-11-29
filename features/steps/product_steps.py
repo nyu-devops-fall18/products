745c2abc839776bd93f260f8fc5bee2ba7e31479
Saumya <sg5290@nyu.edu>
2018-11-28 19:13:39 -0500

    Fixing BDD

--- features/steps/product_steps.py
+++ features/steps/product_steps.py
@@ -135,7 +135,7 @@ def step_impl(context, message):
 
 @then('I should see "{text_string}" in the "{element_name}" field')
 def step_impl(context, text_string, element_name):
-    # element_id = 'product_' + element_name.lower()
+    element_id = 'product_' + element_name.lower()
     # element = context.driver.find_element_by_id(element_id)
     # expect(element.get_attribute('value')).to_equal(text_string)
     found = WebDriverWait(context.driver, WAIT_SECONDS).until(
